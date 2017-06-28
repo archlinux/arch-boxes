421150c36c73da58fdde02fd193824c755542d92
Rafael Soares <rafaelsoaresbr@users.noreply.github.com>
2017-06-28 10:54:45 -0400

    Remove header/dkms (Closes #4)
    Use Virtualbox guest kernel modules for Arch Kernel (virtualbox-guest-modules-arch).

--- provision/virtualbox.sh
+++ provision/virtualbox.sh
@@ -3,6 +3,4 @@
 set -e
 set -x
 
-sudo pacman -S --noconfirm linux-headers
-sudo pacman -S --noconfirm virtualbox-guest-utils-nox
-sudo systemctl enable vboxservice
+sudo pacman -S --noconfirm virtualbox-guest-utils-nox virtualbox-guest-modules-arch

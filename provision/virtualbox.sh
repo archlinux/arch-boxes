e84ebfc3eaa0dbcd3ad80129afae5a3429c6ee42
MBetters <mbetters@udel.edu>
2019-05-17 16:22:16 -0400

    Added a pacman repo update and used 'yes' instead of 'noconfirm' for the package install line

--- provision/virtualbox.sh
+++ provision/virtualbox.sh
@@ -3,5 +3,6 @@
 set -e
 set -x
 
-sudo pacman -S --noconfirm virtualbox-guest-utils-nox virtualbox-guest-modules-arch
+sudo pacman -Syy
+yes | sudo pacman -Syy virtualbox-guest-utils-nox virtualbox-guest-modules-arch
 sudo systemctl enable vboxservice

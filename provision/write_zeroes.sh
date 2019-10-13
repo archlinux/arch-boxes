7b8dfbb90b611f61c0a4ea98674f162a930346ad
Robin B <robin@broda.me>
2019-10-13 14:03:52 +0200

    write_zeroes: flush buffers before writing zeroes
    this avoids spurious failures with e.g. btrfs' metadata backing store when alternative filesystems are used (i.e. #62)

--- provision/write_zeroes.sh
+++ provision/write_zeroes.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 
+# Make sure unwritten data has been flushed beforehand
+sync
 # Write zeros to improve virtual disk compaction.
 zerofile=$(/usr/bin/mktemp /zerofile.XXXXX)
 dd if=/dev/zero of="$zerofile" bs=1M || true

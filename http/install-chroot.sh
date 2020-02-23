6093b90a3078b17804914a8f391559d6d6b397ff
Kristian Klausen <kristian@klausen.dk>
2020-02-23 22:46:16 +0100

    Move common cloud/chroot code to its own script
    Signed-off-by: Kristian Klausen <kristian@klausen.dk>

--- http/install-chroot.sh
+++ http/install-chroot.sh
@@ -1,100 +1,11 @@
 #!/bin/bash
+# shellcheck disable=SC2034
+NEWUSER="vagrant"
 
-set -e
-set -x
-
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
-sed -i -e 's/^#\(en_US.UTF-8\)/\1/' /etc/locale.gen
-locale-gen
-echo 'LANG=en_US.UTF-8' >/etc/locale.conf
-
-# setting vagrant user credentials
-echo -e 'vagrant\nvagrant' | passwd
-useradd -m -U vagrant
-echo -e 'vagrant\nvagrant' | passwd vagrant
-
-# setting automatic authentication for any action requiring admin rights via Polkit
-cat <<EOF >/etc/polkit-1/rules.d/49-nopasswd_global.rules
-polkit.addRule(function(action, subject) {
-    if (subject.isInGroup("vagrant")) {
-        return polkit.Result.YES;
-    }
-});
-EOF
-
-# setting sudo for vagrant user
-cat <<EOF >/etc/sudoers.d/vagrant
-Defaults:vagrant !requiretty
-vagrant ALL=(ALL) NOPASSWD: ALL
-EOF
-chmod 440 /etc/sudoers.d/vagrant
-
-# install vagrant ssh key
-install --directory --owner=vagrant --group=vagrant --mode=0700 /home/vagrant/.ssh
-curl --output /home/vagrant/.ssh/authorized_keys --location https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
-chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys
-chmod 0600 /home/vagrant/.ssh/authorized_keys
-
-# setup network
-cat <<EOF >/etc/systemd/network/eth0.network
-[Match]
-Name=eth0
-
-[Network]
-DHCP=ipv4
-EOF
-
-# Setup pacman-init.service for clean pacman keyring initialization
-cat <<EOF >/etc/systemd/system/pacman-init.service
-[Unit]
-Description=Initializes Pacman keyring
-Wants=haveged.service
-After=haveged.service
-Before=sshd.service
-ConditionFirstBoot=yes
-
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=/usr/bin/pacman-key --init
-ExecStart=/usr/bin/pacman-key --populate archlinux
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Add service for running reflector on first boot
-cat <<EOF >/etc/systemd/system/reflector-init.service
-[Unit]
-Description=Initializes mirrors for the VM
-After=network.target
-Wants=network.target
-ConditionFirstBoot=yes
-
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=reflector --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# enabling important services
-systemctl daemon-reload
-systemctl enable sshd
-systemctl enable haveged
-systemctl enable systemd-networkd
-systemctl enable systemd-resolved
-systemctl enable pacman-init.service
-systemctl enable reflector-init.service
-
-if [ -b "/dev/sda" ]; then
-  grub-install /dev/sda
-elif [ -b "/dev/vda" ]; then
-  grub-install /dev/vda
-fi
-sed -i -e 's/^GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=1/' /etc/default/grub
-# setup unpredictable kernel names
-sed -i -e 's/^GRUB_CMDLINE_LINUX=.*$/GRUB_CMDLINE_LINUX="net.ifnames=0"/' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
+post() {
+  # install vagrant ssh key
+  install --directory --owner=vagrant --group=vagrant --mode=0700 /home/vagrant/.ssh
+  curl --output /home/vagrant/.ssh/authorized_keys --location https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+  chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys
+  chmod 0600 /home/vagrant/.ssh/authorized_keys
+}

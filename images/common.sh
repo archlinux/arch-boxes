e86469e003a27f28b2eb05d5b5c3853cd7b7a009
Kristian Klausen <kristian@klausen.dk>
2023-07-01 03:22:11 +0200

    Update Vagrant public key hash to account for added ed25519 key[1]
    [1] https://github.com/hashicorp/vagrant/commit/b40f6e5fdae1113518e9c92f96dfcd364b646ff9

--- images/common.sh
+++ images/common.sh
@@ -27,7 +27,7 @@ EOF
 install --directory --owner=vagrant --group=vagrant --mode=0700 /home/vagrant/.ssh
 curl --output /home/vagrant/.ssh/authorized_keys --location https://github.com/hashicorp/vagrant/raw/main/keys/vagrant.pub
 # WARNING: Please only update the hash if you are 100% sure it was intentionally updated by upstream.
-sha256sum -c <<< "9aa9292172c915821e29bcbf5ff42d4940f59d6a148153c76ad638f5f4c6cd8b /home/vagrant/.ssh/authorized_keys"
+sha256sum -c <<< "55009a554ba2d409565018498f1ad5946854bf90fa8d13fd3fdc2faa102c1122 /home/vagrant/.ssh/authorized_keys"
 chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 EOF

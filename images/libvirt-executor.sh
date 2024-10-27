68997ce35e7dcd779dcd323bc77ea13e73c92a54
Kristian Klausen <kristian@klausen.dk>
2024-10-27 20:16:23 +0100

    fixup! wip: libvirt-executor image

--- images/libvirt-executor.sh
+++ images/libvirt-executor.sh
@@ -10,6 +10,9 @@ function pre() {
   sed -E 's/^#(IgnorePkg *=)/\1 linux/' -i "${MOUNT}/etc/pacman.conf"
   sed 's/^\(GRUB_CMDLINE_LINUX=".*\)"$/\1 lockdown=confidentiality"/' -i "${MOUNT}/etc/default/grub"
   arch-chroot "${MOUNT}" /usr/bin/grub-mkconfig -o /boot/grub/grub.cfg
+  # We want to use the transient hostname
+  # https://github.com/systemd/systemd/pull/30814
+  rm -f "${MOUNT}/etc/hostname"
 
   cat <<EOF >"${MOUNT}/etc/systemd/network/80-dhcp.network"
 [Match]
